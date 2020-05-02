ff5b8ad0462a6fa99e238f165f19264dfdc55861
geveillard <geveillard@murex.com>
2020-05-02 21:26:11 +0200

    server

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 group = "org.ggye"
 version = "1.0-SNAPSHOT"
 
+val ktor_version = "1.3.1"
+
 repositories {
     mavenCentral()
 }
@@ -12,6 +14,19 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation("com.google.code.gson:gson:2.8.6")
+    
+    //ktor
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-apache:$ktor_version")
+    implementation("io.ktor:ktor-client-auth-basic:$ktor_version")
+    implementation("io.ktor:ktor-auth-jwt:$ktor_version")
+    implementation("io.ktor:ktor-client-json:$ktor_version")
+    implementation("io.ktor:ktor-client-json-jvm:$ktor_version")
+    implementation("io.ktor:ktor-client-gson:$ktor_version")
+    implementation("io.ktor:ktor-gson:$ktor_version")
+    implementation("io.ktor:ktor-client-auth:$ktor_version")
+    implementation("io.ktor:ktor-client-auth-jvm:$ktor_version")
 }
 
 tasks {
---
+++ src/main/kotlin/server.kt
@@ -0,0 +1,65 @@
+import com.google.gson.JsonNull
+import com.google.gson.JsonPrimitive
+import com.google.gson.JsonSerializer
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CORS
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.MissingRequestParameterException
+import io.ktor.gson.gson
+import io.ktor.http.HttpMethod
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.post
+import io.ktor.routing.route
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+import java.lang.IllegalArgumentException
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+fun main() {
+    val parsingResult = loadResultFiles()
+    val analyzer = Analyzer(parsingResult.auctions)
+
+    val server = embeddedServer(Netty, port = 9898) {
+        install(ContentNegotiation) {
+            gson {
+                registerTypeAdapter(LocalDateTime::class.java, JsonSerializer<LocalDateTime> { date, _, _ -> if (date == null) JsonNull.INSTANCE else JsonPrimitive(date.format(
+                    DateTimeFormatter.ISO_LOCAL_DATE_TIME)) })
+                registerTypeAdapter(Instant::class.java, JsonSerializer<Instant> { instant, _, _ -> if (instant == null) JsonNull.INSTANCE else JsonPrimitive(instant.atZone(
+                    ZoneId.of("UTC")).format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)) })
+                setPrettyPrinting()
+            }
+        }
+        install(CORS) {
+            method(HttpMethod.Get)
+            method(HttpMethod.Post)
+            method(HttpMethod.Delete)
+            method(HttpMethod.Put)
+            method(HttpMethod.Patch)
+            anyHost()
+        }
+        
+        routing {
+            route("/items") {
+                get {
+
+                }
+            }
+
+            route("/quotations") {
+                get("/{itemId}") {
+                    val itemId = call.parameters["itemId"]?.toLongOrNull()
+                        ?: throw MissingRequestParameterException("Item id " + call.parameters["itemId"] + " invalid.")
+                    call.respond(HttpStatusCode.OK, analyzer.bestBuyout(itemId))
+                }
+            }
+        }
+    }
+    server.start(wait = true)
+}
